fde3ab915d96d610e2ff4072912a40659f54fe64
AngelicaMVG <angie.velazquez2211@gmail.com>
2017-03-16 11:51:37 -0600

    terminado2

--- assets/js/celsius.js
+++ assets/js/celsius.js
@@ -1,12 +1,12 @@
 function convertirCelsius(){
   var faren = parseInt(document.getElementById('farenheit').value);
   var celsi = document.getElementById('celsius');
-  var celsius = (faren - 32) * 5/9;
+  var celsius = ((faren - 32) * (5/9));
   celsi.value = celsius;
 };
 function convertirFarenheit(){
   var celsi = parseInt(document.getElementById('celsius').value);
   var faren = document.getElementById('farenheit');
-  var farenheit = (celsi * 5/9) + 32;
+  var farenheit = (celsi  + 32) * 9/5 ;
   faren.value = farenheit;
 };
